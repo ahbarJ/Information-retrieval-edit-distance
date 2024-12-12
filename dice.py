eedde7fe29cfc8164e3ad629c25ba1dab43e484f
Ahbar <92169845+ahbarJ@users.noreply.github.com>
2024-12-13 00:15:55 +0330

    Update dice.py add function distance

--- dice.py
+++ dice.py
@@ -3,11 +3,5 @@ def dice_coefficient(set1, set2):
     dice = (2.0 * intersection) / (len(set1) + len(set2))
     return dice
 
-# Example usage
-set1 = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"}
-set2 = {"A", "quick", "brown", "dog", "jumps", "over", "the", "lazy", "fox"}
-
-# Calculate Dice coefficient
-dice_coef = dice_coefficient(set1, set2)
-
-print(f"Dice Coefficient: {dice_coef}")
+def distance (top10, rest):
+    return dice_coefficient(top10, rest)
