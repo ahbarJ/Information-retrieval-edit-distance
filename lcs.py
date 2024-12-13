c0b2e811ebdc043930388a27886af00610ad51a2
Ahbar <92169845+ahbarJ@users.noreply.github.com>
2024-12-13 19:27:48 +0330

    Update lcs.py iterative implementation

--- lcs.py
+++ lcs.py
@@ -1,10 +1,14 @@
-def lcs(X, Y, m, n):
-    if m == 0 or n == 0:
-        return 0
-    elif X[m-1] == Y[n-1]:
-        return 1 + lcs(X, Y, m-1, n-1)
-    else:
-        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n))
+def lcs(X, Y):
+    m, n = len(X), len(Y)
+    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
+
  
 def distance (word1, word2):
     return lcs(word1, word2, len(word1), len(word2))
