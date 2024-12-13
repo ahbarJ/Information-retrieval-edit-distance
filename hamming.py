60dbde88cd8f70600c1f90c9662fc22c7796858e
Ahbar <92169845+ahbarJ@users.noreply.github.com>
2024-12-13 18:48:39 +0330

    Update hamming.py implementaion fix

--- hamming.py
+++ hamming.py
@@ -1,7 +1,7 @@
 def hamming_distance(str1, str2):
     # Make sure the strings are of equal length
     if len(str1) != len(str2):
-        raise ValueError("Input strings must have the same length")
+        return "differing len"
 
     # Calculate Hamming distance
     distance = sum(c1 != c2 for c1, c2 in zip(str1, str2))
