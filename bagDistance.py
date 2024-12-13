d458f1d32427769d265a5dcdb46f42a0253bd2cd
Ahbar <92169845+ahbarJ@users.noreply.github.com>
2024-12-13 18:05:12 +0330

    Update bagDistance.py clean up

--- bagDistance.py
+++ bagDistance.py
@@ -7,47 +7,11 @@ from py_stringmatching import utils
 from py_stringmatching.similarity_measure.sequence_similarity_measure import \
                                                     SequenceSimilarityMeasure
 
-
-[docs]
 class BagDistance(SequenceSimilarityMeasure):
-    """Bag distance measure class.
-    """
     def __init__(self):
         super(BagDistance, self).__init__()
 
-[docs]
     def get_raw_score(self, string1, string2):
-        """
-        Computes the bag distance between two strings.
-
-        For two strings X and Y, the Bag distance is:
-        :math:`max( |bag(string1)-bag(string2)|, |bag(string2)-bag(string1)| )`
-
-        Args:
-            string1,string2 (str): Input strings
-
-        Returns:
-            Bag distance (int)
-
-        Raises:
-            TypeError : If the inputs are not strings
-
-        Examples:
-            >>> bd = BagDistance()
-            >>> bd.get_raw_score('cat', 'hat')
-            1
-            >>> bd.get_raw_score('Niall', 'Neil')
-            2
-            >>> bd.get_raw_score('aluminum', 'Catalan')
-            5
-            >>> bd.get_raw_score('ATCG', 'TAGC')
-            0
-            >>> bd.get_raw_score('abcde', 'xyz')
-            5
-
-        References:
-            * http://www.icmlc.org/icmlc2011/018_icmlc2011.pdf
-        """
         # input validations
         utils.sim_check_for_none(string1, string2)
         utils.sim_check_for_string_inputs(string1, string2)
